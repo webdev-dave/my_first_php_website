020720d5914cfed7d1295ff377b6056e4d0aee90
David Richard <elidovrich.coding@gmail.com>
2024-04-11 15:33:40 -0400

    experimenting

--- index.php
+++ index.php
@@ -1,14 +1,17 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>1st PHP Site</title>
 </head>
+
 <body>
+    <p>This is a <?php echo "awesome"; ?> paragraph!</p>
     <?php
-    
-        echo "Hello World!";
+    echo "This is ALSO a paragraph!";
     ?>
 </body>
+
 </html>
